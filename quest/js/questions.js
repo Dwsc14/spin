085448cc5acfae4a8f82d8ee38339c46bd814951
Nguyễn Đức <ducna1462@gmail.com>
2023-03-07 21:54:21 +0700

    update

---
+++ quest/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Đâu là nhóm rác thuộc nhóm có thể tái chế",
+    answer: "giấy, kim loại, vỏ hộp",
+    options: [
+      "giấy, kim loại, vỏ hộp",
+      "sành sứ, gạch, xỉ than",
+      "rau, củ, quả héo",
+      "Không có đáp án"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Chất thải nào sau đây gây ô nhiễm đất ?",
+    answer: "Bánh mì",
+    options: [
+      "Lá nhôm",
+      "Nhựa",
+      "Bánh mì",
+      "Tất cả ý trên"
+    ]
+  },
+  {
+    numb: 3,
+    question: " Loại chất thải nào phân hủy hoàn toàn khi chôn vào đất",
+    answer: "Chất thải thực vật",
+    options: [
+      "Chất thải thực vật",
+      "Chất thải động vật",
+      "Chất thải nhựa",
+      "Chất thải kim loại"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Đâu là loại rác có thể tái sử dụng",
+    answer: "Structured Query Language",
+    options: [
+      "Chai sữa tắm",
+      "Giấy ướt",
+      "Pin, ắc quy",
+      "Khẩu trang y tế"
+    ]
+  },
+  {
+    numb: 5,
+    question: " Môi trường biển nào nhiều rác thải",
+    answer: "eXtensible Markup Language",
+    options: [
+      "các khu du lịch biển.",
+      "các thành phố cảng, nơi khai thác dầu",
+      "đảo ven bờ.",
+      "các cửa sông."
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
